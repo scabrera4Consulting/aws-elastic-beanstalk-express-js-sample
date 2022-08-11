ad558c03991d71e4f2e99ab31247b212be31f984
GentleTyrant <sebastiancz17@gmail.com>
2022-08-11 15:09:34 -0600

    Segundo cambio para Revisión Manual

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Prueba AWS DevOps'));
+app.get('/', (req, res) => res.send('Cambio para Revisión Manual'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
